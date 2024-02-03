cc41571cea73831489d25b1c6ac1f19ceec45dce
Bimlesh Mandavi <bimleshmandavi0@gmail.com>
2024-02-03 23:49:23 +0530

    add delete cart api in mocks

--- src/mocks/cart.js
+++ src/mocks/cart.js
@@ -32,6 +32,68 @@ class CartApi {
           },
         }
       );
+      console.log("response in get cart", response);
+      if (response.data.status === "SUCCESS") {
+        return response.data;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  async deleteCart(id) {
+    try {
+      const response = await axios.delete(
+        `http://localhost:5003/userapp/cart/soft-delete/${id}`,
+
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+          },
+        }
+      );
+      console.log("response in delete cart", response);
+      if (response.data.status === "SUCCESS") {
+        return response.data;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async listCart(id) {
+    try {
+      let data = {
+        query: {
+          userId: id,
+          isDeleted: false,
+        },
+        options: {
+          collation: "",
+          sort: { name: 1 },
+          populate: "products.productId",
+          projection: "",
+          lean: false,
+          leanWithId: true,
+          page: 1,
+          limit: 10,
+          pagination: true,
+          useEstimatedCount: false,
+          useCustomCountFn: false,
+          forceCountFn: false,
+          read: {},
+          options: {},
+        },
+        isCountOnly: false,
+      };
+      const response = await axios.post(
+        `http://localhost:5003/userapp/cart/list`,
+        data,
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+          },
+        }
+      );
       console.log("response", response);
       if (response.data.status === "SUCCESS") {
         return response.data;
