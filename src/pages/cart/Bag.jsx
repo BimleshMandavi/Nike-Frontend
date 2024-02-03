a371c9b309be1326cb27d12cc0b59e6859dc408e
Bimlesh Mandavi <bimleshmandavi0@gmail.com>
2024-02-03 23:46:58 +0530

    add delete fun.

--- src/pages/cart/Bag.jsx
+++ src/pages/cart/Bag.jsx
@@ -1,67 +1,123 @@
 import "./Bag.css";
 import { IoMdHeartEmpty } from "react-icons/io";
 import { RiDeleteBinLine } from "react-icons/ri";
+import { useDispatch, useSelector } from "react-redux";
+import { deleteCart, listCart } from "../../redux/slices/cart";
+import { useEffect, useState } from "react";
 
 const Bag = () => {
-  const items = {
-    id: "001",
-    image: "img1.png",
-    company: "Carlton London",
-    item_name: "Rhodium-Plated CZ Floral Studs",
-    original_price: 1045,
-    current_price: 606,
-    discount_percentage: 42,
-    return_period: 14,
-    delivery_date: "10 Oct 2023",
-    rating: { stars: 4.5, count: 1400 },
+  const dispatch = useDispatch();
+
+  const [refresh, setRefresh] = useState(false);
+  const { cart } = useSelector((state) => state.cart);
+  const { user } = useSelector((state) => state.auth);
+
+  console.log("user", cart);
+
+  const handleFetchListCart = async () => {
+    try {
+      if (!user?.id) {
+        return;
+      }
+
+      let result = await dispatch(listCart(user?.id));
+      if (result) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch (error) {
+      console.log("error", error);
+    }
   };
+
+  useEffect(() => {
+    handleFetchListCart();
+  }, [refresh]);
 
   const handleFav = () => {
     console.log("item has been added to wishlist");
   };
-  const handleDel = () => {
-    console.log("item has been deleted");
+  const handleDel = async (id) => {
+    let data = await dispatch(deleteCart(id));
+    console.log(data);
+    if (data) {
+      setRefresh(!refresh);
+      return true;
+    } else {
+      return false;
+    }
   };
 
-  
-
-
   return (
-    <div
-      className="bag-container"
-      style={{ height: "55.5vh", overflow: "hidden" }}
-    >
+    <div className="bag-container">
       <div className="bag-heading">
         <p>Bag</p>
       </div>
-      <div className="bag-items">
-        <div className="right-part">
-          <div className="item-imgs">
-            <img src={items.image} />
-          </div>
-          <div className="item-info">
-            <div className="item-title">
-              <a href="/">{items.company}</a>
-            </div>
-            <div className="item-name">{items.item_name}</div>
-            <div className="item-color">Black/White</div>
-
-            <div className="item-size">Size</div>
-
-            <div className="item-quant">Quantity</div>
-            <div className="item-btns">
-              <div className="item-fav">
-                <IoMdHeartEmpty className="btns" onClick={handleFav} />
+      <div
+        style={{
+          maxHeight: "100vh",
+          overflowY: "auto",
+        }}
+      >
+        {cart && cart.length > 0 ? (
+          cart.map((data) => (
+            <div
+              className="bag-items"
+              key={data.id}
+              style={{
+                padding: "10px 0px 10px 0px",
+                borderBottom: "2px solid grey",
+              }}
+            >
+              <div className="right-part">
+                <div className="item-imgs">
+                  <img src={data?.products[0]?.productId?.image} />
+                </div>
+                <div className="item-info">
+                  <div className="item-title">
+                    <div className="flex">
+                      {data?.products[0]?.productId?.title?.longTitle}
+                    </div>
+                  </div>
+                  <div className="item-name">
+                    {data?.products[0]?.productId?.title?.shortTitle}
+                  </div>
+                  <div className="item-color">
+                    Colour: {data?.products[0]?.productId?.subCategory}
+                  </div>
+                  <div className="flex justify-between">
+                    <div className="item-size">Size</div>
+                    <div className="item-quant">
+                      Quantity: {data?.products[0]?.qty}
+                    </div>
+                  </div>
+                  <div className="item-btns gap-8">
+                    <div className="item-fav">
+                      <IoMdHeartEmpty className="btns" onClick={handleFav} />
+                    </div>
+                    <div className="item-del">
+                      <RiDeleteBinLine
+                        className="btns"
+                        onClick={() => handleDel(data?.id)}
+                      />
+                    </div>
+                  </div>
+                </div>
               </div>
-              <div className="item-del">
-                <RiDeleteBinLine className="btns" onClick={handleDel} />
+              <div className="left-part">
+                <span className="item-price">
+                  MRP:{data?.products[0]?.productId?.price?.mrp}
+                </span>
               </div>
             </div>
-          </div>
-        </div>
-        <div className="left-part">
-          <span className="item-price">MRP:{items.original_price}</span>
-        </div>
+          ))
+        ) : (
+          <div>No item in your bag !</div>
+        )}
+      </div>
+      <div className="more-product">
+       
       </div>
     </div>
   );
