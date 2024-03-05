d439b5770177eba776acba496cd24243024d90bb
Bimlesh Mandavi <bimleshmandavi0@gmail.com>
2024-03-05 17:17:59 +0530

    Add new components

---
+++ src/layout/SearchBox.jsx
@@ -0,0 +1,64 @@
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { getProducts } from "../redux/slices/productSlice";
+import PropTypes from "prop-types";
+const SearchBox = ({ handleCToogle }) => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [page, setPage] = useState(1);
+  const [limit, setLimit] = useState(9);
+  const [filter, setFilter] = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      navigate("/products");
+      console.log("searchTerm", searchTerm);
+      setFilter({ "title.longTitle": { $regex: searchTerm, $options: "i" } });
+      handleCToogle();
+    }
+  };
+
+  const fatchProduct = async () => {
+    let result = await dispatch(getProducts(page, limit, filter));
+    if (result) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  useEffect(() => {
+    fatchProduct();
+  }, [filter]);
+  return (
+    <>
+      <div className="searchBar">
+        <input
+          type="text"
+          id="searchInput"
+          placeholder="Search"
+          onChange={(e) => setSearchTerm(e.target.value)}
+          onKeyPress={handleKeyPress}
+          style={{
+            height: "40px",
+
+            borderRadius: "20px",
+            border: "1px solid transparent",
+            background: "#eaeaea",
+            padding: "5px 5px 5px 9px",
+            width: "203px",
+            boxSizing: "border-box",
+            marginLeft: "auto",
+            marginTop: "6px",
+          }}
+        />
+      </div>
+    </>
+  );
+};
+SearchBox.propTypes = {
+  handleCToogle: PropTypes.func.isRequired, // PropTypes validation for handleCloseToogle function
+};
+export default SearchBox;
---
+++ src/layout/SearchToggle.jsx
@@ -0,0 +1,80 @@
+import * as React from "react";
+import Box from "@mui/joy/Box";
+import Drawer from "@mui/joy/Drawer";
+import ButtonGroup from "@mui/joy/ButtonGroup";
+import Divider from "@mui/joy/Divider";
+import SearchIcon from "@mui/icons-material/Search";
+import SearchBox from "./SearchBox";
+export default function DrawerAnchor() {
+  const [state, setState] = React.useState({
+    top: true,
+  });
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+  const handleCToogle = () => {
+    setState(!state);
+  };
+
+  const list = (anchor) => (
+    <Box
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <Divider />
+    </Box>
+  );
+
+  React.useState(() => {}, [setState]);
+
+  return (
+    <React.Fragment>
+      <ButtonGroup variant="outlined">
+        {["top"].map((anchor) => (
+          <SearchIcon
+            key={anchor}
+            onClick={toggleDrawer(anchor, true)}
+            style={{ cursor: "pointer" }}
+          />
+        ))}
+      </ButtonGroup>
+      {["top"].map((anchor) => (
+        <Drawer
+          className="h-[35px]"
+          key={anchor}
+          anchor={anchor}
+          open={state[anchor]}
+          onClose={toggleDrawer(anchor, false)}
+        >
+          {list(anchor)}
+          <div className="flex justify-between px-6 pt-7">
+            <SearchBox
+              className="absolute right-8"
+              handleCToogle={handleCToogle}
+            />
+            <button
+              style={{
+                width: "20%",
+                height: "48px",
+                borderRadius: "30px",
+                cursor: "pointer",
+              }}
+              onClick={handleCToogle}
+            >
+              Cencel
+            </button>
+          </div>
+        </Drawer>
+      ))}
+    </React.Fragment>
+  );
+}
