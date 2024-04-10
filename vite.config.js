a29cb25475d99144ebfb8113bccebc36a20dc6fc
Bimlesh Mandavi <bimleshmandavi0@gmail.com>
2024-04-10 20:48:43 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  // Load env file based on mode in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the VITE_ prefix.
+  // eslint-disable-next-line no-undef
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react()],
+    // vite config
+    define: {
+      ...Object.keys(env).reduce((prev, key) => {
+        prev[`process.env.${key}`] = JSON.stringify(env[key])
+        return prev
+      }, {}),
+    },
+  }
 })
